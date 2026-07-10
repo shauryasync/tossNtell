ec993200436b48ded58062fa7c446acf1f69daf5
Shaurya <edshaurya@gmail.com>
2026-07-10 15:26:33 +0530

    set up express server & connect MongoDB

---
+++ server/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+export default app;
