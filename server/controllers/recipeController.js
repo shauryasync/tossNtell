9153b5bfeae7631589c9bc4856a98c418aac9018
Shaurya <edshaurya@gmail.com>
2026-07-10 22:33:24 +0530

    Add recipe CRUD with search, filtering, sorting and pagination

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -45,10 +45,70 @@ const createRecipe = async (req, res) => {
 
 const getRecipes = async (req, res) => {
   try {
-    const recipes = await Recipe.find().populate("createdBy", "name email");
+    const { search, category, cuisine, difficulty, maxTime, sort } = req.query;
+    const filter = {};
 
-    res.status(200).json(recipes);
+    // Search by title
+    if (search) {
+      filter.title = {
+        $regex: search,
+        $options: "i",
+      };
+    }
+
+    // Category Filter
+    if (category) {
+      filter.category = category.toLowerCase();
+    }
+
+    // Cuisine Filter
+    if (cuisine) {
+      filter.cuisine = {
+        $regex: cuisine,
+        $options: "i",
+      };
+    }
+
+    // Difficulty Filter
+    if (difficulty) {
+      filter.difficulty = difficulty.toLowerCase();
+    }
+
+    // Cooking Time Filter
+    if (maxTime) {
+      filter.cookingTime = {
+        $lte: Number(maxTime),
+      };
+    }
+    let sortOption = { createdAt: -1 };
+
+    if (sort === "oldest") {
+      sortOption = { createdAt: 1 };
+    }
+
+    if (sort === "time") {
+      sortOption = { cookingTime: 1 };
+    }
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+
+    const skip = (page - 1) * limit;
+    const recipes = await Recipe.find(filter)
+      .populate("createdBy", "name email")
+      .sort(sortOption)
+      .skip(skip)
+      .limit(limit);
+    const totalRecipes = await Recipe.countDocuments(filter);
+
+    res.status(200).json({
+      totalRecipes,
+      currentPage: page,
+      totalPages: Math.ceil(totalRecipes / limit),
+      recipes,
+    });
   } catch (error) {
+    console.error(error);
+
     res.status(500).json({
       message: "Server Error",
     });
